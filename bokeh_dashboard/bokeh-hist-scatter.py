a6ba2e4c128f8cbcb31b883ceca325497e9d0f26
Moritz Gnisia <moritz.gnisia@statworx.com>
2018-03-19 10:58:58 +0100

    scatterplot

---
+++ bokeh_dashboard/bokeh-hist-scatter.py
@@ -0,0 +1,38 @@
+import numpy as np
+from seaborn import load_dataset
+from bokeh.io import curdoc
+from bokeh.layouts import row
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+
+# Festlegen des Dashboard Titles
+curdoc().title = "Histogramm und Scatter Plot"
+
+# Datenset laden
+tips = load_dataset("tips")
+
+# Histogramm mit Numpy erstellen
+top_hist, x_hist = np.histogram(tips.total_bill)
+
+# Daten in Dict überführen
+source_hist = ColumnDataSource(data=dict(x=x_hist[:-1], top=top_hist))
+
+# Allgemeinen Plot erstellen
+hist = figure(plot_height=400, plot_width=400, title="Histogramm",
+              tools="crosshair,pan,reset,save,wheel_zoom")
+
+# Darstellung des Histograms
+hist.vbar(x='x', top='top', width=2, source=source_hist)
+
+# Allgemeinen Plot erstellen
+scatter = figure(plot_height=400, plot_width=400, title="Scatter-Plot",
+                 tools="crosshair,pan,reset,save,wheel_zoom",
+                 x_axis_label='total_bill',
+                 y_axis_label='tip')
+
+source_scatter = ColumnDataSource(data=dict(x=tips.total_bill, y=tips.tip))
+# Darstellung des Scatter Diagramms
+scatter.scatter(x='x', y='y', source=source_scatter)
+
+# Hinzufügen des Histograms in das Hauptdokument
+curdoc().add_root(row(hist, scatter, width=800))
