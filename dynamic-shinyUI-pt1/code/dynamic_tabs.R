690454cdbb6ca9850bae7fa6cec9d0998fafa63d
Oliver Guggenbühl <oliver.guggenbuehl@statworx.com>
2019-07-22 14:34:21 +0200

    add advanced script

---
+++ dynamic-shinyUI-pt1/code/dynamic_tabs.R
@@ -0,0 +1,48 @@
+library(shiny)
+library(shinydashboard)
+library(glue)
+
+ui <- dashboardPage(
+  dashboardHeader(),
+  
+  dashboardSidebar(
+    sliderInput(inputId = "slider", label = NULL, min = 1, max = 5, value = 3, step = 1)
+  ),
+  
+  dashboardBody(
+    fluidRow(
+      box(width = 12,
+          p(mainPanel(width = 12,
+                      column(width = 6, uiOutput("reference")),
+                      column(width = 6, uiOutput("comparison"))
+          )
+          )
+      )
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$reference <- renderUI({
+    tabsetPanel(
+      tabPanel(
+        "Reference",
+        h3("Reference Content"))
+    )
+  })
+  
+  output$comparison <- renderUI({
+    req(input$slider)
+    
+    myTabs <- lapply(1:input$slider, function(i) {
+      
+      tabPanel(title = glue("Tab {i}"),
+               h3(glue("Content {i}"))
+      )
+    })
+    do.call(tabsetPanel, myTabs)
+  })
+}
+
+shinyApp(ui = ui, server = server)
